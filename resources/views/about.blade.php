bd955c0d2742536203ce2201b62daf3d388037ff
Jasa-Hollow <joel2262000@gmail.com>
2020-12-23 14:33:21 -0600

    Blade tests

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,44 +1,34 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Hereda la plantilla definida. -->
+@extends('layouts.template')
 
-        <title>About</title>
+<!-- ............................................... -->
+<!-- Encabezado -->
+@section('header')
+    <h1 class="uk-heading-small header-text">About</h1>
+    <p>
+        Mi nombre es Jasa, y soy un programador super novato.
+    </p>
+@endsection
+<!-- ............................................... -->
 
-        <!-- Estilo CSS -->
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/uikit.min.css') }}">
+<!-- ............................................... -->
+<!-- Primera tarjeta -->
+@section('card1')
+    <h2 class="header-text">Index</h2>
+    <p>Página principal</p>
+    <a href="/">
+        <button class="uk-button uk-button-primary">Go!</button>
+    </a>
+@endsection
+<!-- ............................................... -->
 
-        <!-- Script JS -->
-        <script src="{{ asset('js/uikit.min.js') }}"></script>
-
-    </head>
-    <body>
-        <div class="uk-container uk-text-center">
-            <h1 class="uk-heading-small header-text">About</h1>
-            <p>Mi nombre es Jasa, y soy un programador super novato.
-            </p>
-            <section uk-grid>
-                <div class="uk-width-1-2@m">
-                    <div class="uk-card uk-card-default uk-card-body">
-                        <h2 class="header-text">Index</h2>
-                        <p>Página principal</p>
-                        <a href="/">
-                            <button class="uk-button uk-button-primary">Go!</button>
-                        </a>
-                    </div>
-                </div>
-                <div class="uk-width-1-2@m">
-                    <div class="uk-card uk-card-default uk-card-body">
-                        <h2 class="header-text">Contact</h2>
-                        <p>Contáctame así es</p>
-                        <a href="/contact">
-                            <button class="uk-button uk-button-primary">Go!</button>
-                        </a>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </body>
-</html>
+<!-- ............................................... -->
+<!-- Segunda tarjeta -->
+@section('card2')
+    <h2 class="header-text">Contact</h2>
+    <p>Contáctame así es</p>
+    <a href="/contact">
+        <button class="uk-button uk-button-primary">Go!</button>
+    </a>
+@endsection
+<!-- ............................................... -->
